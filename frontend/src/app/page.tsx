7b9a9020b8d1e7f6e4b817d9bdf78950ff5791da
Dep <109551168+dave-andrew@users.noreply.github.com>
2024-06-27 23:04:00 +0700

    refac: prettify docker implementation & seperate development production env

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,21 +2,17 @@ import Image from "next/image";
 import { BackgroundBeams } from "./components/aceternity-ui/BackgroundBeams";
 import HomeHero from "./pages/home/HomeHero";
 import HomeAbout from "./pages/home/HomeAbout";
-import HomeDataset from "./pages/home/HomeDataset";
-import PageTransition from "./components/transitions/PageTransition";
 
-export enum HomeElementEnum {
+export enum HomeElementEnum{
   ABOUT = 'about',
 }
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-dark-background">
-
+    <main className="flex min-h-screen flex-col items-center justify-between bg-background">
       <HomeHero />
+      
       <HomeAbout />
-      <PageTransition />
-      <HomeDataset />
 
     </main>
   );
