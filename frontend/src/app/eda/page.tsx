de9266a02d672944ff5136fa6a59d118058ae133
StefanieSheryl <85792580+StefanieSheryl@users.noreply.github.com>
2024-06-29 19:08:30 +0700

    adjust eda

--- frontend/src/app/eda/page.tsx
+++ frontend/src/app/eda/page.tsx
@@ -8,14 +8,23 @@ export default function Model() {
             <div className="flex flex-col min-h-screen items-center w-full h-full py-32">
                 <h3 className="text-5xl font-medium text-light-accent">Data Exploration</h3>
                 <div className="flex flex-col min-h-screen items-center w-full h-full m-8 gap-4">
-                    <Card title="testing" description="testinggg" imagePath="/assets/images/team/WIN_20230504_16_36_44_Pro.jpg"/>
-                    <Card title="testing" description="testinggg" imagePath="/assets/images/team/WIN_20230504_16_36_44_Pro.jpg"/>
-                    <Card title="testing" description="testinggg" imagePath="/assets/images/team/WIN_20230504_16_36_44_Pro.jpg"/>
-                    <Card title="testing" description="testinggg" imagePath="/assets/images/team/WIN_20230504_16_36_44_Pro.jpg"/>
-                    <Card title="testing" description="testinggg" imagePath="/assets/images/team/WIN_20230504_16_36_44_Pro.jpg"/>
-                    <Card title="testing" description="testinggg" imagePath="/assets/images/team/WIN_20230504_16_36_44_Pro.jpg"/>
-                    <Card title="testing" description="testinggg" imagePath="/assets/images/team/WIN_20230504_16_36_44_Pro.jpg"/>
-                    <Card title="testing" description="testinggg" imagePath="/assets/images/team/WIN_20230504_16_36_44_Pro.jpg"/>
+                    <Card title="data.head()" description="Displaying Initial Data" imagePath="/assets/images/EDA/data_head.jpg"/>
+                    <Card title="data.info()" description="Displaying Data Information" imagePath="/assets/images/EDA/data_info.jpg"/>
+                    <Card title="data.describe()" description="Data Summary Statistics" imagePath="/assets/images/EDA/data_desc.jpg"/>
+                    <Card title="np.round(data.isnull().mean() * 100, 1)" description="Displaying Data Null Percentage" imagePath="/assets/images/EDA/data_null.jpg"/>
+                    <Card title="data.var()" description="Displaying Data Variance" imagePath="/assets/images/EDA/data_var.jpg"/>
+                    <Card title="data.hist()" description="Displaying Histogram for Each Column" imagePath="/assets/images/EDA/hist.jpg"/>
+                    <Card title="sns.boxplot()" description="To See Outlier Data" imagePath="/assets/images/EDA/box.jpg"/>
+                    <Card title="data.corr()" description="To See The Correlation" imagePath="/assets/images/EDA/corr.jpg"/>
+                </div>
+                <h3 className="text-5xl font-medium text-light-accent">Data Visualization</h3>
+                <div className="flex flex-col min-h-screen items-center w-full h-full m-8 gap-4">
+                    <Card title="sns.histplot()" description="Displaying Plot by Drug Usage" imagePath="/assets/images/EDA/hist_ic.jpg"/>
+                    <Card title="sns.FacetGrid()" description="Display FacetGrid with Hist of the NScore for different combinations of Gender and Education" imagePath="/assets/images/EDA/face.jpg"/>
+                    <Card title="sns.kdeplot()" description="Display KDE Plot for Numerical Features" imagePath="/assets/images/EDA/kde.jpg"/>
+                    <Card title="sns.violinplot()" description="Displaying Violin Plot for Categorical Features" imagePath="/assets/images/EDA/violin.jpg"/>
+                    <Card title="TSNE()" description="Displaying TSNE for each categorical feature" imagePath="/assets/images/EDA/tsne.jpg"/>
+                    <Card title="sns.heatmap(data.corr())" description="Displaying Heatmap" imagePath="/assets/images/EDA/heatmap.jpg"/>
                 </div>
             </div>
         </main>
